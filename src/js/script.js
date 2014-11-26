bd3ddb773c04a8b598f0da33570b71eb47ef0314
agtoaaa <andylee@chimerik.com>
2014-11-25 16:45:59 -0800

    Updated  (function($, window, document) {
    (function($, window, document) {
    
    })(jQuery, window, document);
    
    Wrap your JS in this instead of using $(document).ready()

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,13 @@
-/*this is the top script*/
-
-$(document).ready(function() {
+(function($, window, document) {
     $(".expand-list").click(function() {
         $(this).find(".object").slideToggle();
     });
+
+
+
+$(document).ready(function(){
+
+
     var $root = $('html, body');
     $('a').click(function() {
         if ($.attr(this, 'href').indexOf("#") != -1 && $.attr(this, 'href') != "#") {
@@ -13,140 +17,139 @@ $(document).ready(function() {
             return false;
         }
     });
-
 });
 
-$(window).scroll(function() {
-    $('.middle-master').each(function() {
-        var imagePos = $(this).offset().top;
+    
 
-        var topOfWindow = $(window).scrollTop();
-        if (imagePos < topOfWindow + 400) {
-            $(this).addClass("slideUp");
-        }
+    /*start animation*/
+    $(window).scroll(function() {
+        $('.middle-master').each(function() {
+            var imagePos = $(this).offset().top;
+
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 400) {
+                $(this).addClass("slideUp");
+            }
+        });
+        $('.color-belt').each(function() {
+            var imagePos = $(this).offset().top;
+
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 400) {
+                $(this).addClass("expandOpen");
+            }
+        });
     });
-    $('.color-belt').each(function() {
-        var imagePos = $(this).offset().top;
+    $(window).scroll(function() {
+        $('.knowhow').each(function() {
+            var imagePos = $(this).offset().top;
 
-        var topOfWindow = $(window).scrollTop();
-        if (imagePos < topOfWindow + 400) {
-            $(this).addClass("expandOpen");
-        }
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 400) {
+                $(this).addClass("slideUp");
+            }
+        });
     });
-});
-$(window).scroll(function() {
-    $('.knowhow').each(function() {
-        var imagePos = $(this).offset().top;
+    $(window).scroll(function() {
+        $('.bluemaster').each(function() {
+            var imagePos = $(this).offset().top;
 
-        var topOfWindow = $(window).scrollTop();
-        if (imagePos < topOfWindow + 400) {
-            $(this).addClass("slideUp");
-        }
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 400) {
+                $(this).addClass("slideLeft");
+            }
+        });
+        $('.livein').each(function() {
+            var imagePos = $(this).offset().top;
+
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 400) {
+                $(this).addClass("slideRight");
+            }
+        });
+        $('.first-second-content').each(function() {
+            var imagePos = $(this).offset().top;
+
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 400) {
+                $(this).addClass("slideLeft");
+            }
+        });
     });
-});
-$(window).scroll(function() {
-    $('.bluemaster').each(function() {
-        var imagePos = $(this).offset().top;
+    $(window).scroll(function() {
+        $('.cantmiss').each(function() {
+            var imagePos = $(this).offset().top;
 
-        var topOfWindow = $(window).scrollTop();
-        if (imagePos < topOfWindow + 400) {
-            $(this).addClass("slideLeft");
-        }
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 400) {
+                $(this).addClass("slideLeft");
+            }
+        });
     });
-    $('.livein').each(function() {
-        var imagePos = $(this).offset().top;
+    $(window).scroll(function() {
+        $('.cantmiss-2').each(function() {
+            var imagePos = $(this).offset().top;
 
-        var topOfWindow = $(window).scrollTop();
-        if (imagePos < topOfWindow + 400) {
-            $(this).addClass("slideRight");
-        }
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 400) {
+                $(this).addClass("slideRight");
+            }
+        });
     });
-    $('.first-second-content').each(function() {
-        var imagePos = $(this).offset().top;
+    $(window).scroll(function() {
+        $('.perfectmen').each(function() {
+            var imagePos = $(this).offset().top;
 
-        var topOfWindow = $(window).scrollTop();
-        if (imagePos < topOfWindow + 400) {
-            $(this).addClass("slideLeft");
-        }
-    });
-});
-$(window).scroll(function() {
-    $('.cantmiss').each(function() {
-        var imagePos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 400) {
+                $(this).addClass("slideRight");
+            }
+        });
+        $('.testi').each(function() {
+            var imagePos = $(this).offset().top;
 
-        var topOfWindow = $(window).scrollTop();
-        if (imagePos < topOfWindow + 400) {
-            $(this).addClass("slideLeft");
-        }
-    });
-});
-$(window).scroll(function() {
-    $('.cantmiss-2').each(function() {
-        var imagePos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 400) {
+                $(this).addClass("slideDown");
+            }
+        });
+        $('.doctorsay').each(function() {
+            var imagePos = $(this).offset().top;
 
-        var topOfWindow = $(window).scrollTop();
-        if (imagePos < topOfWindow + 400) {
-            $(this).addClass("slideRight");
-        }
-    });
-});
-$(window).scroll(function() {
-    $('.perfectmen').each(function() {
-        var imagePos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 400) {
+                $(this).addClass("slideDown");
+            }
+        });
+        $('.tri-work').each(function() {
+            var imagePos = $(this).offset().top;
 
-        var topOfWindow = $(window).scrollTop();
-        if (imagePos < topOfWindow + 400) {
-            $(this).addClass("slideRight");
-        }
-    });
-    $('.testi').each(function() {
-        var imagePos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 400) {
+                $(this).addClass("slideDown");
+            }
+        });
+        $('.intro').each(function() {
+            var imagePos = $(this).offset().top;
 
-        var topOfWindow = $(window).scrollTop();
-        if (imagePos < topOfWindow + 400) {
-            $(this).addClass("slideDown");
-        }
-    });
-    $('.doctorsay').each(function() {
-        var imagePos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 400) {
+                $(this).addClass("slideDown");
+            }
+        });
+        $('.core-value').each(function() {
+            var imagePos = $(this).offset().top;
 
-        var topOfWindow = $(window).scrollTop();
-        if (imagePos < topOfWindow + 400) {
-            $(this).addClass("slideDown");
-        }
-    });
-    $('.tri-work').each(function() {
-        var imagePos = $(this).offset().top;
-
-        var topOfWindow = $(window).scrollTop();
-        if (imagePos < topOfWindow + 400) {
-            $(this).addClass("slideDown");
-        }
-    });
-    $('.intro').each(function() {
-        var imagePos = $(this).offset().top;
-
-        var topOfWindow = $(window).scrollTop();
-        if (imagePos < topOfWindow + 400) {
-            $(this).addClass("slideDown");
-        }
-    });
-    $('.core-value').each(function() {
-        var imagePos = $(this).offset().top;
-
-        var topOfWindow = $(window).scrollTop();
-        if (imagePos < topOfWindow + 400) {
-            $(this).addClass("slideUp");
-        }
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 400) {
+                $(this).addClass("slideUp");
+            }
+        });
     });
     $('.arrow').mouseover(function() {
-    $(this).addClass("hatch");
-});
-$('.arrow').mouseout(function() {
-    $(this).removeClass("hatch");
-});
-
-});
-
-
-
+        $(this).addClass("hatch");
+    });
+    $('.arrow').mouseout(function() {
+        $(this).removeClass("hatch");
+    });
+})(jQuery, window, document);
